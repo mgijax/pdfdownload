5c4b472503c960a00387aebccf0ea36a2c0898f8
Jonathan Beal <jsb@jax.org>
2017-09-07 07:59:58 -0400

    switched csh to sh; added archiving of log files

---
+++ download_plos.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/sh
+
+# Usage:  download_plos.csh
+#
+# History
+#
+# jsb	09/04/2017
+
+cd `dirname $0`
+. ./Configuration
+
+# if we don't have a log directory, create one
+if [ ! -d ${PDFDOWNLOADLOGDIR} ]; then
+	mkdir ${PDFDOWNLOADLOGDIR}
+fi
+ 
+# remove log files older than 90 days
+find ${PDFDOWNLOADLOGDIR}/* -type f -mtime +90 -exec rm -rf {} \;
+
+# if we have a current log file, rename it with a date/time and start a new one
+LOG=${PDFDOWNLOADLOGDIR}/`basename $0`.log
+if [ -e $LOG ]; then
+	mv $LOG $LOG.`date '+%Y%m%d.%H%M'` | tee -a ${LOG}
+fi
+touch $LOG
+
+date | tee -a ${LOG}
+
+# Run the downloader script for default timeframe
+
+./download_plos.py $1 $2 | tee -a ${LOG}
+
+date | tee -a ${LOG}
