b615762df8eafeb2b344b8e7d57dc54f47a2a458
Lori Corbani <lori.corbani@jax.org>
2026-06-29 12:28:18 -0400

    wts2-1869/bulk download, retrieve PMC pdfs using PMID ID (or PMCID) list

--- pmc/getPMCByList.py
+++ pmc/getPMCByList.py
@@ -1,8 +1,5 @@
 #
-# For testing boto3 with PMC
-#
-# to test from command line:
-# aws --no-sign-request s3 cp s3://pmc-oa-opendata/PMC12930826.1/PMC12930826.1.pdf .
+# Get PMC by a List
 #
 
 import boto3
@@ -12,21 +9,18 @@ from botocore.config import Config
 # 1. Initialize S3 client without credentials for public access
 s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
 
-bucket_name = 'pmc-oa-opendata'
-# 2. Example Key: OA_comm/all/PMC7xxxxxx/PMC7123456.pdf
-#object_key = 'PMC12930826.1/PMC12930826.1.pdf' 
-#object_key = 'PMC12930826.1/PMC12930826.1.pdf' 
+# copy to WTS2 ticket folder
+local_file_name = '/mgi/all/wts2_projects/1800/WTS2-1869/' + tokens[0] + '.pdf'
 
+# use a list of pmid,pmc (40475700,PMC12138534)
 fp = open('PMC.csv', 'r')
 for line in fp.readlines():
     tokens = line[:-1].split(',')
     object_key = tokens[1] + '.1/' + tokens[1] + '.1.pdf' 
-    local_file_name = '/mgi/all/wts2_projects/1800/WTS2-1869/' + tokens[0] + '.pdf'
-    print(object_key, tokens[1], local_file_name)
+    #print(object_key, tokens[1], local_file_name)
 
     try:
-       s3.download_file(bucket_name, object_key, local_file_name)
+       s3.download_file('pmc-oa-opendata, object_key, local_file_name)
     except:
-       print('could not find:', object_key)
-
+       print(object_key)
 fp.close()
